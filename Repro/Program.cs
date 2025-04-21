762270bd3bf21c3d33004f3646c68a48bef9b8cc
Mark Adamson <3154635+mungojam@users.noreply.github.com>
2025-04-21 22:44:51 +0100

    Working but not trimmable example

---
+++ Repro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+using Windows.Win32.Foundation;
+using Windows.Win32.Networking.WinInet;
+
+var uri = "https://dummy.internal";
+
+var provider = (IProofOfPossessionCookieInfoManager)new ProofOfPossessionCookieInfoManager();
+
+unsafe
+{
+    fixed (char* pMessage = uri)
+    {
+        var uriPointer = new PCWSTR(pMessage);
+        ProofOfPossessionCookieInfo* cookieInfoPtr = null;
+        provider.GetCookieInfoForUri(uriPointer, out var cookieInfoCount, &cookieInfoPtr);
+
+        Marshal.FreeCoTaskMem((nint)cookieInfoPtr);
+
+        Console.WriteLine($"cookieInfoCount: {cookieInfoCount}");
+    }
+}
